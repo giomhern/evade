1f90836654ef54a56b2477c93520d0f1205e01a1
giovannimhern <giovannimhern@uchicago.edu>
2024-04-13 22:05:44 -0500

    Finished: title screen, movement, and sound fx

--- player.gd
+++ player.gd
@@ -7,19 +7,18 @@ var screen_size
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO
-	if Input.is_action_just_pressed("move_right"):
+	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_just_pressed("move_left"):
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1	
-	if Input.is_action_just_pressed("move_up"):
+	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-	if Input.is_action_just_pressed("move_down"):
+	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 	
 	if velocity.length() > 0:
@@ -44,4 +43,11 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	pass # Replace with function body.
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false 
